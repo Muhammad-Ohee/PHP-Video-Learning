e42c1111214f72ef1e4dc1cd08d909b547df8744
Muhammad-Ohee <md.ohee786@gmail.com>
2023-10-16 23:18:21 +0600

    version 1

--- module 3 - Mastering PHP Array and String/intro array.php
+++ module 3 - Mastering PHP Array and String/intro array.php
@@ -16,9 +16,9 @@ for ($i = 0; $i < $n; $i++){
     echo $student[$i] . "\n";
 }
 
-// for ($i = $n-1; $i >= 0; $i--){
-//     echo $student[$i] . "\n";
-// }
+for ($i = $n-1; $i >= 0; $i--){
+    echo $student[$i] . "\n";
+}
 
 
 
